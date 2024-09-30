6c306fad30ef85b596af921f9b31ea73d2bf3b91
D3ffyduck <bhuvi007@protonmail.com>
2024-09-30 15:08:22 +0530

    done new updates

---
+++ vinoveproject/android/app/src/main/kotlin/com/example/vinoveproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vinoveproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ vinoveproject/lib/key.dart
@@ -0,0 +1,4 @@
+class ApiKey {
+  static String api =
+      "896529s9s85s6896685111000599s5895248bcd9f06be7b04cf496ef6662ae11da85";
+}
